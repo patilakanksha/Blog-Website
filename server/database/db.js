ab2fadff75605d10554f4255fc0703b7065c5d18
patilakanksha <53122583+patilakanksha@users.noreply.github.com>
2021-11-30 19:55:31 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -6,7 +6,7 @@ const Connection = async () =>{
 
    try
    {
-        const URL='mongodb://akanksha:akanksha!1@blogweb-shard-00-00.m4qyr.mongodb.net:27017,blogweb-shard-00-01.m4qyr.mongodb.net:27017,blogweb-shard-00-02.m4qyr.mongodb.net:27017/Blog?ssl=true&replicaSet=atlas-gx20ux-shard-0&authSource=admin&retryWrites=true&w=majority'
+        const URL='mongodb://user:pass@blogweb-shard-00-00.m4qyr.mongodb.net:27017,blogweb-shard-00-01.m4qyr.mongodb.net:27017,blogweb-shard-00-02.m4qyr.mongodb.net:27017/Blog?ssl=true&replicaSet=atlas-gx20ux-shard-0&authSource=admin&retryWrites=true&w=majority'
     
          mongoose.connect(URL,{ useNewUrlParser : true, useUnifiedTopology: true});
          console.log("connection created with database");
